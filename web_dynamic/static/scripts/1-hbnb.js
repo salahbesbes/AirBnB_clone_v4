26aa019126aaa1adc20803bc369bd4f69a1d1e7a
salahbesbes <salah_besbes@ymail.com>
2021-05-14 16:56:08 +0200

    get attribute from tag put amenity names to h4 tag

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -11,7 +11,21 @@ $(document).ready(() => {
     let checked = thisInput.is(':checked');
     if (checked) amenitySelected[id] = name;
     else delete amenitySelected[id];
+    /** create a string of names */
+    let Newh4Contenent = '';
+    for (key in amenitySelected) {
+      /** check if the width of h4 is filled or not yet */
+      if (33 - Newh4Contenent.length > 0) {
+        Newh4Contenent += amenitySelected[key] + ', ';
+      } else {
+        Newh4Contenent = Newh4Contenent.slice(0, 33) + '.....';
+        break;
+      }
+    }
+    /** remove last 2 char */
+    Newh4Contenent = Newh4Contenent.slice(0, -2);
     /** fill the H4 tag with names */
-    console.log(amenitySelected);
+    let h4Tag = $(this).closest('.amenities').find('h4');
+    h4Tag.text(Newh4Contenent);
   });
 });
